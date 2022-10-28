4c3eb5279bbb1d86b6ed540e8298c1de0eb7bf7c
pbnazrin <pbnazrin@gmail.com>
2022-10-28 10:10:04 +0530

    added_functionalities_to buttons in object palette

--- src/app/components/canvas/canvas.component.ts
+++ src/app/components/canvas/canvas.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
-  styleUrls: ['./canvas.component.css']
+  styleUrls: ['./canvas.component.css'],
 })
 export class CanvasComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
--- src/app/components/object-palette/object-palette.component.ts
+++ src/app/components/object-palette/object-palette.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { CanvasComponent } from '../canvas/canvas.component';
+import 'fabric';
+declare let fabric: any;
 
 @Component({
+  providers: [CanvasComponent],
   selector: 'app-object-palette',
   templateUrl: './object-palette.component.html',
-  styleUrls: ['./object-palette.component.css']
+  styleUrls: ['./object-palette.component.css'],
 })
 export class ObjectPaletteComponent implements OnInit {
+  public canvas: any;
 
-  constructor() { }
+  public shape: any;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit() {
+    console.log('ngOnit');
+    this.canvas = new fabric.Canvas('canvas', {});
+    // console.log(this.canvas);
+    console.log(this);
   }
 
+  rectClickEvent() {
+    console.log('rectClick');
+    // this.canvas = new fabric.Canvas('canvas', {});
+    console.log(this);
+    //this.canvas = new fabric.Canvas('canvas', {});
+    this.shape = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'transparent',
+      width: 50,
+      height: 50,
+      stroke: '#000',
+    });
+
+    this.canvas.add(this.shape);
+  }
+
+  triClickEvent() {
+    this.shape = new fabric.Triangle({
+      left: 120,
+      top: 120,
+      width: 100,
+      height: 50,
+      fill: 'transparent',
+      stroke: '#000',
+    });
+
+    this.canvas.add(this.shape);
+  }
+  circleClickEvent() {
+    this.shape = new fabric.Circle({
+      left: 140,
+      top: 140,
+      radius: 50,
+      fill: 'transparent',
+      height: 50,
+      stroke: '#000',
+    });
+
+    this.canvas.add(this.shape);
+  }
 }
